f528986b266633055014a878d2a04586f77e0f81
Wyatt Scherer <wscherer25@gmail.com.com>
2026-09-13 19:01:42 -0400

    Added index page and main:

--- server.py
+++ server.py
@@ -17,3 +17,10 @@ def sent_detector():
     """
 
     return message
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
