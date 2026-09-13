c551a153c7cd96c01e603e521be71150e2dbbc9d
Wyatt Scherer <wscherer25@gmail.com.com>
2026-09-13 18:58:00 -0400

    Added sent_detector function

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    message = f"""
+    For the given statement, the system response is
+    'anger': {response['anger']}, 'disgust': {response['disgust']},
+    'fear': {response['fear']}, 'joy': {response['joy']}, and
+    'sadness: {response['sadness']}. The dominant emotion is \033[1m{response['dominant_emotion']}\033[03m]
+    """
+
+    return message
